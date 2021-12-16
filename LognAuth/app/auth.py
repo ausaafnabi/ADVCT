a0c17736d6e6f60fd03dad5d91cb080c57844ebc
ausaafnabi <nabiausaaf@gmail.com>
2021-12-16 19:21:15 +0530

    Changed The auth response

--- LognAuth/app/auth.py
+++ LognAuth/app/auth.py
@@ -65,14 +65,63 @@ def login():
 		if error is None:
 			timestamp = time.ctime()
 			authkey = generate_password_hash(user['username']+timestamp)
-			message = {'status': 200,'auth_Key':authkey, 'timestamp': timestamp, 'message':'Successfully authorized'}
+			message = {'status': 200,'auth_Key':authkey,'user': user['username'], 'timestamp': timestamp, 'message':'Successfully authorized'}
 		response = {
 				'is_Successful': True if error is None else False,
 				'response' : message if error is None else error
 			}
 	return response
 
+@bp.route('/validate',methods=('GET','POST'))
+def validation():
+	if request.method == 'POST':
+		# to change for api
+		response = request.json['response']
+		error = None
+		if not check_password_hash(response['auth_Key'],response['user']+response['timestamp']):
+			error = {'status':404}
+		else:
+			resp = {'status':200}
+		response = {
+				'is_Successful': True if error is None else False,
+				'response' : resp if error is None else error
+		}
+	return response
+	
 @bp.route('/logout',methods=('GET','POST'))
 def logout():
 	authkey = None
 	return {'status':200,'message':'reset authkey'}
+
+
+'''
+#Basic Auth Example
+
+from functools import wraps
+from flask import request
+from flask import Flask
+
+app = Flask(__name__)
+def login_required(f):
+    @wraps(f)
+    def wrapped_view(**kwargs):
+        auth = request.authorization
+        if not (auth and check_auth(auth.username, auth.password)):
+            return ('Unauthorized', 401, {
+                'WWW-Authenticate': 'Basic realm="Login Required"'
+            })
+        return f(**kwargs)
+    return wrapped_view
+
+def check_auth(username,password):
+	if username=="X" and password =="Y":
+		return True
+	else:
+		return False
+
+@app.route('/secret')
+@login_required
+def secret():
+    return f'Logged in as {request.authorization.username}.'
+
+'''
